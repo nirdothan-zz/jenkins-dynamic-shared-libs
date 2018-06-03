95d94a7df656bcd4d331876b3820863504109c5a
Nir Dothan <nird@qwilt.com>
2018-06-03 20:11:10 +0300

    test

--- com/qwilt/cq/jenkins/Utils.groovy
+++ com/qwilt/cq/jenkins/Utils.groovy
@@ -1,7 +1,7 @@
 package com.qwilt.cq.jenkins
 class Utils implements Serializable {
   def steps
-  Utilities(steps) {this.steps = steps}
+  Utils(steps) {this.steps = steps}
   def echo(args) {
     steps.sh "echo 'hello NIRNIRNIR'"
   }
