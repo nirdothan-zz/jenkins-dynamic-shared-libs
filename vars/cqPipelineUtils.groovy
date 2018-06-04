4a0d55eef66026235911263098b9de9df30237c6
Nir Dothan <nird@qwilt.com>
2018-06-04 17:08:12 +0300

    Update cqPipelineUtils.groovy

--- vars/cqPipelineUtils.groovy
+++ vars/cqPipelineUtils.groovy
@@ -2,5 +2,13 @@
 
 def call(String name = 'human') {
   echo "Hello, ${name}."
+  env.AWS_REGION = 'us-west-2'
+  env.AWS_EC2_BASE_IMAGE = 'ami-3a3b5742'
+  env.AWS_EC2_INSTANCE_PROFILE = 'cq-packer-role'
+  env.AWS_VPC = 'vpc-6730b31e'
+  env.AWS_VPC_SUBNET = 'subnet-b0110bf8'
+  // The ID of a secret defined in jenkins holding the aws access key id and secret
+  env.AWS_CREDS_JENKINS_SECRET_ID='gradle-aws-access-key'
+  
 }
 
