c80d81c56fc8ce1d4f78d32f3bd078f82c28b268
Nir Dothan <nird@qwilt.com>
2018-06-04 16:09:29 +0300

    Update Utils.groovy

--- src/com/qwilt/cq/jenkins/Utils.groovy
+++ src/com/qwilt/cq/jenkins/Utils.groovy
@@ -24,7 +24,7 @@ class Utils implements Serializable {
   
   def myenv(){
     def mybool=false
-    [true, false,false, mybool]
+    [a: true, b:false,c:false, d:mybool]
   }
   
   def map(Map config){
