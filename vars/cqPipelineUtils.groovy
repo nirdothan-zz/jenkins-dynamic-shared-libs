d3dd836e1ac2ce96e9043b37fb90dd6d234b84f6
Nir Dothan <nird@qwilt.com>
2018-06-05 11:28:30 +0300

    Update cqPipelineUtils.groovy

--- vars/cqPipelineUtils.groovy
+++ vars/cqPipelineUtils.groovy
@@ -9,6 +9,7 @@ def call(String name = 'human') {
   env.AWS_VPC_SUBNET = 'subnet-b0110bf8'
   // The ID of a secret defined in jenkins holding the aws access key id and secret
   env.AWS_CREDS_JENKINS_SECRET_ID='gradle-aws-access-key'
+  echo "param value in funtion is ${params.param1}"
   
 }
 
